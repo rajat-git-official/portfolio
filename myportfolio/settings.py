5891c415150a7f7ca1ff816025400594adba2c7d
rajat-git-official <rajat@maaze.in>
2024-06-02 16:11:43 +0530

    Addedchanges

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -115,6 +115,15 @@ USE_I18N = True
 USE_TZ = True
 
 
+# settings.py
+
+ALLOWED_HOSTS = [
+    'portfolio-i2ne.onrender.com',  # Add your Render domain
+    '127.0.0.1',
+    'localhost',
+]
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
